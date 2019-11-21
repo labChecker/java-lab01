2c02328afbd4850897e7df58f90401a16508c252
Yurii Matora <yura.matora11@gmail.com>
2019-11-21 23:24:43 +0200

    Initial commit

---
+++ src/main/java/com/kpi/fict/DefaultStudentService.java
@@ -0,0 +1,28 @@
+package com.kpi.fict;
+
+public class DefaultStudentService implements StudentService {
+    @Override
+    public void task1() {
+        throw new UnsupportedOperationException("Need to make implementation");
+    }
+
+    @Override
+    public void task2() {
+        throw new UnsupportedOperationException("Need to make implementation");
+    }
+
+    @Override
+    public void task3() {
+        throw new UnsupportedOperationException("Need to make implementation");
+    }
+
+    @Override
+    public void task4() {
+        throw new UnsupportedOperationException("Need to make implementation");
+    }
+
+    @Override
+    public void task5() {
+        throw new UnsupportedOperationException("Need to make implementation");
+    }
+}
---
+++ src/main/java/com/kpi/fict/StudentService.java
@@ -0,0 +1,13 @@
+package com.kpi.fict;
+
+public interface StudentService {
+    void task1();
+
+    void task2();
+
+    void task3();
+
+    void task4();
+
+    void task5();
+}
