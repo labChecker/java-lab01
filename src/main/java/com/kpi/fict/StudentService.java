77b873fb175ad67abf8ef0575d679c8d2e47da82
Yurii Matora <yura.matora11@gmail.com>
2019-11-25 21:34:57 +0200

    Add tasks for variant

--- src/main/java/com/kpi/fict/StudentService.java
+++ src/main/java/com/kpi/fict/StudentService.java
@@ -1,13 +1,17 @@
 package com.kpi.fict;
 
+import com.kpi.fict.entities.Student;
+
+import java.util.List;
+
 public interface StudentService {
-    void task1();
+    List<Student> findStudentsWithoutExams();
 
-    void task2();
+    List<Student> findStudentsWhoTakeMathAndOneMoreExam();
 
-    void task3();
+    List<Student> findStudentsWithRatingMoreThanAvgAndTakeMathExam();
 
-    void task4();
+    Student findStudentWithMaxAvgExamRating();
 
-    void task5();
+    List<Student> findStudentsWithMathRatingMoreThanAvgAndTakeEngExam();
 }
