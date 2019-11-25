935a3ea172c137566d86ac30ad92b91667e67bff
Yurii Matora <yura.matora11@gmail.com>
2019-11-25 23:05:26 +0200

    Add tasks for variant

--- src/main/java/com/kpi/fict/DefaultStudentService.java
+++ src/main/java/com/kpi/fict/DefaultStudentService.java
@@ -1,6 +1,9 @@
 package com.kpi.fict;
 
+import com.kpi.fict.entities.Student;
 import com.kpi.fict.repositories.StudentRepository;
+
+import java.util.List;
 
 public class DefaultStudentService implements StudentService {
     private StudentRepository studentRepository;
@@ -10,27 +13,28 @@ public class DefaultStudentService implements StudentService {
     }
 
     @Override
-    public void task1() {
+    public Student findFirstWithoutMath() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
     @Override
-    public void task2() {
+    public List<Student> findStudentsWhoTakeMathAndOneMoreExam() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
     @Override
-    public void task3() {
+    public List<Student> findStudentsWithRatingMoreThanAvgAndTakeMathExam() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
     @Override
-    public void task4() {
+    public double findAvgRatingOfMathExam() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
+    //Delimiter: ','
     @Override
-    public void task5() {
+    public List<String> calculateAvgRatingForEachStudent() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
