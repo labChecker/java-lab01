5b25f9371cb6bdee07fa1621d8872aecc391470a
Yurii Matora <yura.matora11@gmail.com>
2019-11-25 21:47:54 +0200

    Add tasks for variant

--- src/main/java/com/kpi/fict/DefaultStudentService.java
+++ src/main/java/com/kpi/fict/DefaultStudentService.java
@@ -1,6 +1,9 @@
 package com.kpi.fict;
 
+import com.kpi.fict.entities.Student;
 import com.kpi.fict.repositories.StudentRepository;
+
+import java.util.List;
 
 public class DefaultStudentService implements StudentService {
     private StudentRepository studentRepository;
@@ -10,27 +13,28 @@ public class DefaultStudentService implements StudentService {
     }
 
     @Override
-    public void task1() {
+    public List<Student> findStudentsWithoutExams() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
     @Override
-    public void task2() {
+    public List<Student> findStudentsWhoTakeEngExamWith11RatingOrMore() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
     @Override
-    public void task3() {
+    public List<Student> findTwoStudentsWithMaxEngRating() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
     @Override
-    public void task4() {
+    public List<Student> findStudentsWhoTakeMathEngExamWith180RatingOrMore() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
+    //Delimiter: ','
     @Override
-    public void task5() {
+    public List<String> getExamSumAndRatingForEachStudent() {
         throw new UnsupportedOperationException("Need to make implementation");
     }
 
--- src/main/java/com/kpi/fict/StudentService.java
+++ src/main/java/com/kpi/fict/StudentService.java
@@ -1,13 +1,17 @@
 package com.kpi.fict;
 
+import com.kpi.fict.entities.Student;
+
+import java.util.List;
+
 public interface StudentService {
-    void task1();
+    List<Student> findStudentsWithoutExams();
 
-    void task2();
+    List<Student> findStudentsWhoTakeEngExamWith11RatingOrMore();
 
-    void task3();
+    List<Student> findTwoStudentsWithMaxEngRating();
 
-    void task4();
+    List<Student> findStudentsWhoTakeMathEngExamWith180RatingOrMore();
 
-    void task5();
+    List<String> getExamSumAndRatingForEachStudent();
 }
