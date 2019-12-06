97a61ebaf865cb968a86be8847f07d216f88217c
kraftwerk28 <kefirchik3@gmail.com>
2019-12-06 10:44:30 +0200

    add: impl for 12 var

--- src/main/java/com/kpi/fict/DefaultStudentService.java
+++ src/main/java/com/kpi/fict/DefaultStudentService.java
@@ -1,9 +1,11 @@
 package com.kpi.fict;
 
+import com.kpi.fict.entities.Exam;
 import com.kpi.fict.entities.Student;
 import com.kpi.fict.repositories.StudentRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DefaultStudentService implements StudentService {
     private StudentRepository studentRepository;
@@ -14,28 +16,89 @@ public class DefaultStudentService implements StudentService {
 
     @Override
     public Student findStudentWithMaxAvgExamRating() {
-        throw new UnsupportedOperationException("Need to make implementation");
+        return studentRepository
+                .findAll()
+                .stream()
+                .min((a, b) -> {
+                    double r1 = a.getRating();
+                    double r2 = b.getRating();
+                    return Double.compare(r1, r2);
+                })
+                .get();
     }
 
     @Override
     public List<Student> findStudentsWithMathRatingMoreThanAvgAndTakeEngExam() {
-        throw new UnsupportedOperationException("Need to make implementation");
+        double avg = studentRepository
+                .findAll()
+                .stream()
+                .map(Student::getRating)
+                .reduce(Double::sum)
+                .get() / studentRepository.findAll().size();
+        return studentRepository
+                .findAll()
+                .stream()
+                .filter(s ->
+                        s.getRating() > avg &&
+                        s.getExams().stream().anyMatch(e -> e.getType() == Exam.Type.ENGLISH))
+                .collect(Collectors.toList());
     }
 
     //Delimiter: ','
     @Override
     public List<String> getExamSumAndRatingForEachStudent() {
-        throw new UnsupportedOperationException("Need to make implementation");
+        return studentRepository
+                .findAll()
+                .stream()
+                .map(s -> {
+                    double sum = s
+                            .getExams()
+                            .stream()
+                            .map(e -> e.getScore())
+                            .reduce(Double::sum)
+                            .get();
+                    return sum + "," + s.getRating() + "," + s.getName();
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Student> findStudentsWhoTakeMathEngExamWith180RatingOrMore() {
-        throw new UnsupportedOperationException("Need to make implementation");
+        return studentRepository
+                .findAll()
+                .stream()
+                .filter(s -> s
+                        .getExams()
+                        .stream()
+                        .filter(e -> e.getType() == Exam.Type.MATH || e.getType() == Exam.Type.ENGLISH)
+                        .allMatch(e -> e.getScore() > 180)
+                )
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Student> findTwoStudentsWithMaxEngRating() {
-        throw new UnsupportedOperationException("Need to make implementation");
+        return studentRepository
+                .findAll()
+                .stream()
+                .sorted((a, b) -> {
+                    double aa = a
+                            .getExams()
+                            .stream()
+                            .filter(e -> e.getType() == Exam.Type.ENGLISH)
+                            .findFirst()
+                            .get()
+                            .getScore();
+                    double bb = b
+                            .getExams()
+                            .stream()
+                            .filter(e -> e.getType() == Exam.Type.ENGLISH)
+                            .findFirst()
+                            .get()
+                            .getScore();
+                    return Double.compare(aa, bb);
+                })
+                .collect(Collectors.toList());
     }
 
     public StudentRepository getStudentRepository() {
